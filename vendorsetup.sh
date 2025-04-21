8eb770419d08e5e6ea91cce69f6edb894c93a8f8
xSlazharx <creamylofly@gmail.com>
2025-04-21 19:57:02 +0800

    vendorsetup: update
    Signed-off-by: xSlazharx <creamylofly@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,46 +1,14 @@
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2024 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
-FDEVICE="tanzanite"
-# set -o xtrace
+#!/usr/bin/env bash
 
-fox_get_target_device() {
-    local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-    if [ -n "$chkdev" ]; then
-        FOX_BUILD_DEVICE="$FDEVICE"
-    else
-        chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-        [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-    fi
-}
+FOX_MANIFEST_ROOT=$(gettop)
+if [[ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox.mk ]]; then
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-    fox_get_target_device
-fi
-
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export LC_ALL="C.UTF-8"
     export ALLOW_MISSING_DEPENDENCIES=true
 
     # OFR build settings & info
     export FOX_BUILD_DEVICE="tanzanite"
-    export FOX_VERSION="R12.1_0_A14"
+    export FOX_VERSION="R12.1-v1"
     export FOX_VARIANT="HyperOS"
     export FOX_BUILD_TYPE="Unofficial"
     export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
