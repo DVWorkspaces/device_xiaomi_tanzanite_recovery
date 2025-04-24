20df40a3664f82e10ebd8d0c873e3e0f3d3005b9
linastorvaldz <creamylofly@gmail.com>
2025-04-24 12:08:59 +0000

    tanzanite: rewrite orangefox build flags
    Signed-off-by: linastorvaldz <creamylofly@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,36 +1,60 @@
 #!/usr/bin/env bash
-
 export LC_ALL="C.UTF-8"
 export ALLOW_MISSING_DEPENDENCIES=true
 
-# OFR build settings & info
-export FOX_BUILD_DEVICE="tanzanite"
-export FOX_MAINTAINER_PATCH_VERSION="r1"
+export FOX_TARGET_DEVICES="tanzanite"
+export FOX_MAINTAINER_PATCH_VERSION="1"
 export FOX_VARIANT="HyperOS"
-export FOX_BUILD_TYPE="Unofficial"
+
+# OFR Build settings
+export FOX_VENDOR_BOOT_RECOVERY=1
 export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-export FOX_VENDOR_BOOT_RECOVERY=1
+export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
+export FOX_USE_UPDATED_MAGISKBOOT=1
+export FOX_ENABLE_APP_MANAGER=1
+
+export OF_FLASHLIGHT_ENABLE=1
+export OF_NO_REFLASH_CURRENT_ORANGEFOX=1
+export OF_FORCE_CASEFOLDING=1
+export OF_USE_LZ4_COMPRESSION=1
+export OF_USE_GREEN_LED=0
+export OF_FORCE_USE_RECOVERY_FSTAB=1
+export OF_MAINTAINER="eraselk"
+
+# Disable unused stuffs
+export FOX_NO_SAMSUNG_SPECIAL=1
 export FOX_DELETE_MAGISK_ADDON=1
 export FOX_DELETE_AROMAFM=1
 export FOX_REMOVE_AAPT=1
-export FOX_ENABLE_APP_MANAGER=1
-export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+export FOX_REMOVE_BASH=1
 
-# OFR binary files
-export FOX_USE_BASH_SHELL=1
+# OFR Binaries
 export FOX_USE_NANO_EDITOR=1
 export FOX_USE_TAR_BINARY=1
 export FOX_USE_SED_BINARY=1
 export FOX_USE_XZ_UTILS=1
-export FOX_ASH_IS_BASH=1
-export OF_ENABLE_LPTOOLS=1
+
+export OF_ENABLE_ALL_PARTITION_TOOLS=1
 
 # OTA
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
+
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
+export OF_DYNAMIC_FULL_SIZE=9126805504
 
 # Flashlight
 export OF_FL_PATH1=/sys/class/leds/flashlight/brightness
 export OF_FL_PATH2=/sys/class/leds/torch-light0/brightness
+
+# OFR GUI Settings
+export OF_SCREEN_H=2400
+export OF_STATUS_H=95
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=1
+
